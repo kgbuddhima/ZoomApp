3fdde2ea89414bb9bc7cad352715013c94fe6fcc
Buddhima Kudagama <kgbuddhima@hotmail.com>
2020-06-24 13:00:08 +0530

    Add functionality

--- ZoomApp/ZoomApp/MainPage.xaml.cs
+++ ZoomApp/ZoomApp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZoomApp.Common;
+using ZoomApp.Services;
 
 namespace ZoomApp
 {
@@ -13,9 +15,42 @@ namespace ZoomApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        IZoomService zoomService;
         public MainPage()
         {
             InitializeComponent();
+            zoomService = DependencyService.Get<IZoomService>();
+            zoomService.InitZoomLib(Constants.AppKey, Constants.AppSecret);
+        }
+
+        private void btnStart_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!zoomService.IsInitialized())
+                {
+                    return;
+                }
+
+                zoomService.JoinMeeting(txtMeetingId.Text, txtPssword.Text, txtDisplayName.Text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void btnEnd_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                zoomService.LeaveMeeting();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
